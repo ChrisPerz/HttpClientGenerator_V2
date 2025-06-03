f1dcbc0b07088c033f4f8cac230d09e7c09c9337
Christian Perez <cristianperez028@gmail.com>
2025-06-02 20:57:22 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder= WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+
+        // define middleware usage
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+        app.MapControllers();
+
+
+        // Task.Run(() => app.RunAsync());
+        // Task.Delay(1000);
+        // await new ClientGenerator.GenerateClient();
+
+        app.Run();
+    }
+}
+
+
